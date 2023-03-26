7c206aadfdda25235b9d94ee69561b4a0e419461
Gaurav10997 <avdhika@gmail.com>
2023-03-27 01:48:13 +0530

    home-leaderboard

--- src/component/home/Widgets/HomeleaderBoard.jsx
+++ src/component/home/Widgets/HomeleaderBoard.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import "./HomeLeaderBoard.css"
+import RankNormal from './RankNormal'
 function HomeLeaderBoard() {
+  
   return (
     <div className="HomeLeaderBoard-main">
-        <div className="HomeLeaderBoard-div">  
-              LeaderBoard
+        <div className="HomeLeaderBoard-heading">  
+              <h1>LeaderBoard</h1>
         </div>
 
         <div className="HomeLeaderBoard-rankers">
-            
+              <div className="HomeLeaderBoard-rankers-rank2">
+                  <img  src="https://cdn-icons-png.flaticon.com/512/5934/5934990.png" alt="dgadf" width={45} />
+                  <h2>Rank 2</h2>
+              </div>
+              <div className="HomeLeaderBoard-rankers-rank1">
+                  <img  src="https://cdn-icons-png.flaticon.com/512/5934/5934990.png" alt="dgadf" width={45} />
+                  <h2>Rank 1</h2>
+              </div>
+              <div className="HomeLeaderBoard-rankers-rank2">
+                  <img  src="https://cdn-icons-png.flaticon.com/512/5934/5934990.png" alt="dgadf" width={45} />
+                  <h2>Rank 3</h2>
+              </div>
         </div>
 
-        <div className="HomeLeaderBoard-normal">
+        <div className="HomeLeaderBoard-rankers-normal">
+            <RankNormal></RankNormal>
+            <RankNormal></RankNormal>
+            <RankNormal></RankNormal>
+            <RankNormal></RankNormal>
+            <RankNormal></RankNormal>
+            <RankNormal></RankNormal>
+            <RankNormal></RankNormal>
+
 
         </div>
     </div>
---
+++ src/component/questionsList/AllQuestions.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function AllQuestions() {
+  return (
+    <>
+    <div className="QuestionsList__main__Questions">
+        
+        <h2>1.  Greeting from The Pink Community</h2>
+        <button className='QuestionsList__main__Questions__btn'>Solve</button>
+    </div>
+    </>
+
+  )
+}
+
+export default AllQuestions
