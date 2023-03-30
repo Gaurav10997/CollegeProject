b9542eb712d9b993a78f0b284008181df7aded97
Gaurav10997 <avdhika@gmail.com>
2023-03-31 05:01:47 +0530

    compiler

---
+++ src/component/compiler/Congratulations.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Congratulations.css"
+
+function Congratulations() {
+  return (
+    <div className="congratulations"> 
+        <img src="https://media.tenor.com/wcNVGStwbicAAAAC/kapil-balhara.gif" alt="" width={590} />
+    </div>
+    
+  )
+}
+
+export default Congratulations
