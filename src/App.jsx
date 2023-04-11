11b4d39e8a381f1be7f05b6a3f27e0e4cc2f7467
Gaurav10997 <avdhika@gmail.com>
2023-04-12 00:53:35 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { BrowserRouter } from 'react-router-dom'
-import { Route ,Routes  } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 import LeaderBoard from './component/LeaderBoard/LeaderBoard'
 import Home from './component/home/Home'
 import Practise from './component/practiseDsa/Practise'
 import Contest from './component/Contest/Contest'
-import './App.css'
+import Compiler from "./component/compiler/Compiler"
+import Solve from './component/Solvation/Solve'
+import Login from "./component/LoginandSignup/Login"
+import { BrowserRouter } from 'react-router-dom'
+import { Route ,Routes  } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import AuthContext from './store/AuthContest'
-
+import './App.css'
 function App() {
-
-  //  typeof(authCtx.isLoggedIn);
-
-
   return (
     <>
-    {/* <BrowserRouter>
-    <Routes>
+    <BrowserRouter>
+     <Routes>
       <Route path='/' element= {<Home></Home>}/>
       <Route path='/practise' element= { <Practise></Practise>} />
       <Route path='/leaderBoard' element= {<LeaderBoard/>}/>
-      <Route path='/contest' element= {<Contest></Contest>}/>
-     
-    </Routes>  
-    </BrowserRouter> */}
-    <Login></Login>
-    {/* <Compiler></Compiler> */}
+       <Route path='/contest' element= {<Contest></Contest>}/>
+       <Route path='/solve' element= {<Solve></Solve>}/>
+       <Route path='/compiler' element= {<Compiler></Compiler>}/>
+       <Route path='/login' element= {<Login></Login>}/>
+     </Routes>  
+     </BrowserRouter>
     </>
-  
   )
 }
-
 export default App
