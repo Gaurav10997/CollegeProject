3e0d720b323a87a7f279c31d786ed950d8f2c5e4
Gaurav10997 <avdhika@gmail.com>
2023-04-11 07:43:33 +0530

    Access Token

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { BrowserRouter } from 'react-router-dom'
-import { Route ,Routes  } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import LeaderBoard from './component/LeaderBoard/LeaderBoard'
-import Home from './component/home/Home'
-import Compiler from './component/compiler/Compiler'
-import Practise from './component/practiseDsa/Practise'
-import Contest from './component/Contest/Contest'
+// import { BrowserRouter } from 'react-router-dom'
+// import { Route ,Routes  } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+// import LeaderBoard from './component/LeaderBoard/LeaderBoard'
+// import Home from './component/home/Home'
+// import Compiler from './component/compiler/Compiler'
+// import Practise from './component/practiseDsa/Practise'
+// import Contest from './component/Contest/Contest'
+import { useContext } from 'react'
 import Login from './component/LoginandSignup/login'
 import './App.css'
+import AuthContext from './store/AuthContest'
 function App() {
+
+  //  typeof(authCtx.isLoggedIn);
+
+
   return (
     <>
-    <BrowserRouter>
+    {/* <BrowserRouter>
     <Routes>
       <Route path='/' element= {<Home></Home>}/>
       <Route path='/practise' element= { <Practise></Practise>} />
@@ -20,7 +26,8 @@ function App() {
       <Route path='/compiler' element={<Compiler></Compiler>}/>
       <Route path='/login' element={<Login></Login>}/>
     </Routes>  
-    </BrowserRouter>
+    </BrowserRouter> */}
+    <Login></Login>
     </>
   
   )
--- src/component/LoginandSignup/Login.jsx
+++ src/component/LoginandSignup/Login.jsx
@@ -1,55 +1,81 @@
 import React from 'react'
-import Loginviacard from "./Loginviacard"
+import Loginviacard from './Loginviacard'
+import { useState ,useRef , useContext } from 'react'
 import "./login.css"
-import { useState ,useRef } from 'react'
+import AuthContext from '../../store/AuthContest';
 
 function Login() {
-  const [newUser, setNewUser] = useState(false)
-    // function handleclick(){
-    //     setNewUser((prev)=>!prev)
-    // }
-    // if(newUser){
-    //     const handleuser = (e) =>{
-    //         e.preventDefault();
-    //         console.log("clicked login ");
-    //     }
-    // }else{
-
-    //     const handleuser = () =>{
-    //         console.log("clicked signup");
-    //     }
-    // }
-    const enteredEmailref = useRef();
-    const enteredPasswordref = useRef();
-    function handleAdduser(e){
-        e.preventDefault()
-        const enteredEmail=enteredEmailref.current.value;
-        const enteredPassword=enteredPasswordref.current.value;
-        console.log(enteredEmail);
+  const [newUser, setNewUser] = useState(false);
+  const enteredEmailref = useRef();
+  const enteredPasswordref = useRef();
+  const [errorMessage, setErrorMessage] = useState("");
+  const authCtx = useContext(AuthContext)
+  function handleAdduser(e) {
+    e.preventDefault();
+    const enteredEmail = enteredEmailref.current.value;
+    const enteredPassword = enteredPasswordref.current.value;
+    const userInfo = {
+      email: enteredEmail,
+      password: enteredPassword,
+      returnSecureToken: true
+    };
+    let url;
+    if (!newUser) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBlvwokXZ0SoPCq00Mtg-Usj4z0ByGwlpg";
+    } else {
+      url="https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBlvwokXZ0SoPCq00Mtg-Usj4z0ByGwlpg"
     }
-
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(userInfo),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then((res)=>{
+      return res.json();
+    })
+    .then((data)=>{
+      authCtx.login(data.idToken);
+    })
+    // .then((res) => {
+    //   if (res.ok) {
+        
+    //   } else {
+    //     res.json().then((data) => {
+    //       setErrorMessage(data.error.message);
+    //     });
+    //   }
+    // });
+          
+    }
+    function toggleAuth(){
+      setNewUser((prev)=>!prev)
+    }
   return (
     <>
     <div className='login__page'>
      <img className='login__pageimage' src="https://lightoj.com/img/illustrations/questions/programming.svg" alt="" />
      <form className="login__form" onSubmit= {handleAdduser}  >
-    <h1>Welcome Back</h1>
-    <p>Sign in with credentials or social account</p>
+    {!newUser && <h1>Sign In</h1>}
+    {newUser && <h1>Sign Up </h1>}
+    {!newUser && <p>Welcome back to the coolest community in India!</p>}
+    {newUser && <p>Welcome to the coolest community in India! </p>}
     <label htmlFor="Email">
     <input ref={enteredEmailref}  name="Email" type="email" placeholder="Username or Email" className="login__form__inputs"  />
     </label>
     <label  htmlFor="Password"> 
         <input ref={enteredPasswordref} className="login__form__inputs" name="Password" type="text" placeholder="Enter Your Password" />
     </label>
-
-    <button  className='login__form__submitbtn'  >Login</button>
+    <button  className='login__form__submitbtn' onClick={handleAdduser} >Login</button>
     <Loginviacard></Loginviacard>
     <Loginviacard></Loginviacard>
-    <p>Dont Have an account ? </p>{newUser && <p>login</p>}
+    {!newUser &&<p >Dont Have an account ? <b onClick={toggleAuth} >Sign Up</b></p>}
+    {newUser &&<p>Already Part of The Community? <b onClick={toggleAuth} >Sign In</b> </p>}
    </form>
     </div>
-     
-    
+  
     </>
   
   )
