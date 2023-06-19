c67decfb23851587e613c465446a65395d619281
gaurav mandal <avdhika@gmail.com>
2023-06-19 23:44:26 +0530

    deleted unwanted file

--- PinkCommunityBackend/src/main/java/com/pinkcommunity/code/repositories/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pinkcommunity.code.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pinkcommunity.code.entities.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer> {
-
-}
